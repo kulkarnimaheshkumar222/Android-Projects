8157c0abec4856b679f6fe2ebb80dd6086452059
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2022-10-23 23:00:29 +0530

    add actions to button

--- My Diary/app/src/main/java/com/mahesh/mydiary/MainActivity.kt
+++ My Diary/app/src/main/java/com/mahesh/mydiary/MainActivity.kt
@@ -2,10 +2,38 @@ package com.mahesh.mydiary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var userName: EditText
+    private lateinit var password: EditText
+    private lateinit var signInButton: Button
+    private  lateinit var createNewAccountButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        userName = findViewById(R.id.userNameEditTextId)
+        password = findViewById(R.id.passwordEditTextId)
+        signInButton = findViewById(R.id.logInBtnId)
+        createNewAccountButton = findViewById(R.id.createNewAccountBtnId)
+
     }
+
+    fun logInBtnClicked(view: View) {
+        val mToast = Toast.makeText(applicationContext,"Log In clicked", Toast.LENGTH_SHORT)
+        mToast.show()
+    }
+
+    fun createNewAccountBtnClicked(view: View) {
+        val mToast = Toast.makeText(applicationContext,"Create New clicked", Toast.LENGTH_SHORT)
+        mToast.show()
+    }
+
+
 }
